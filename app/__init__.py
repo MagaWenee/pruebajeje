59aa33e9531a78fc7816d64d9ae30620e2050d77
MagaWenee <101905372+MagaWenee@users.noreply.github.com>
2022-10-19 21:41:51 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ class Ping:
     async def on_get(self, req, resp):
         """ Handles GET requests """
         resp.status = falcon.HTTP_200
-        resp.media = dict(ping="pong")
+        resp.media = dict(ping="PONG")
 
 api = falcon.asgi.App()
 api.add_route(f"{V1}/ping", Ping())
